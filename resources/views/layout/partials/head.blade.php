d8c4e41bac27dbf698e2eb30ef61aa4c0807013a
egolubev <135797531>
2021-09-28 23:48:45 +0300

    Загрузка проекта

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<meta name="description" content="">
+
+<meta name="author" content="">
+
+<title>Album example for Bootstrap</title>
+
+<!-- Bootstrap core CSS -->
+
+<link rel="stylesheet" href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css">
+
+<!-- Custom styles for this template -->
+
+<link href="/css/album.css" rel="stylesheet">
