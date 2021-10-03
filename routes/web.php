69829880392a9d5f3651de7426d116bc3a3cbaf1
egolubev <eugeny.golubev@gmail.com>
2021-10-03 12:30:53 +0300

    test commit, статистика

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $articles = \App\Models\Articles::all();
-
     return view('mainpage', [
         'articles' => $articles
     ]);
